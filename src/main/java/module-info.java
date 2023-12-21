effa7e9eaa6ea47ae67d0015df0fabd83abb4af5
Jim Sellers <jim.sellers@ised-isde.gc.ca>
2023-12-21 14:26:59 -0500

    adjusting module file to address warning

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,9 @@ module com.github.sellersj.helloworld.javafx.jpackage {
 
     requires javafx.controls;
 
-    opens com.example.jpackagejavafx to javafx.graphics;
+    requires transitive javafx.graphics;
+
+    // opens com.example.jpackagejavafx to javafx.graphics;
 
     // the package name of this maven project
     exports com.example.jpackagejavafx;
